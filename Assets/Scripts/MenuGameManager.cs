4038c369dc3650c99a16674e25e2ee4dbd3871b6
xavieracaling <xavier.acaling@narrasoft.com>
2024-06-25 04:41:51 +0800

    pre game menu update

--- Assets/Scripts/MenuGameManager.cs
+++ Assets/Scripts/MenuGameManager.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using DG.Tweening;
+using UnityEngine.UI;
 public class MenuGameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_InputField InputX;
+    public TMP_InputField InputY;
+    public Button StartGameBTN;
+    public void PlayGame()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
